9eb1db0dc782ac0576233f0b048a28f151fef03e
Mark Hildebrand <mhildebrand@ucdavis.edu>
2021-10-07 17:17:46 -0700

    Checkpointing

--- src/ReuseDistance.jl
+++ src/ReuseDistance.jl
@@ -1,5 +1,149 @@
 module ReuseDistance
 
-# Write your package code here.
+mutable struct Node{T}
+    key::T
+    priority::UInt
+    left::Node{T}
+    right::Node{T}
+
+    function Node{T}(key::T) where {T}
+        priority = rand(UInt)
+        node = new{T}(key, priority)
+        node.left = node
+        node.right = node
+        return node
+    end
+end
+
+Node(key::T) where {T} = Node{T}(key)
+
+function Base.show(io::IO, node::Node{T}) where {T}
+    hasleft = lchild(node) !== nothing
+    hasright = rchild(node) !== nothing
+    print(io, "Node{$T}($(node.key), $(node.priority), $hasleft, $hasright)")
+    return nothing
+end
+
+_lchild(n::Node) = n.left
+_rchild(n::Node) = n.right
+hasleft(n::Node) = _lchild(n) !== n
+hasright(n::Node) = _rchild(n) !== n
+
+lchild(n::Node) = (n.left === n) ? nothing : _lchild(n)
+rchild(n::Node) = (n.right === n) ? nothing : _rchild(n)
+
+setleft!(n::Node, v) = (n.left = v)
+clearleft!(n::Node) = (n.left = n)
+setright!(n::Node, v) = (n.right = v)
+clearright!(n::Node) = (n.right = n)
+
+function rotate_right!(root::Node)
+    newroot = _lchild(root)
+    hasright(newroot) ? setleft!(root, _rchild(newroot)) : clearleft!(root)
+    setright!(newroot, root)
+    return newroot
+end
+
+function rotate_left!(root::Node)
+    newroot = _rchild(root)
+    hasleft(newroot) ? setright!(root, _lchild(newroot)) : clearright!(root)
+    setleft!(newroot, root)
+    return newroot
+end
+
+function Base.insert!(root::Node, node)
+    if node.key < root.key
+        left = lchild(root)
+        left = setleft!(root, left === nothing ? node : insert!(left, node))
+
+        if left.priority > root.priority
+            root = rotate_right!(root)
+        end
+    else
+        right = rchild(root)
+        right = setright!(root, right === nothing ? node : insert!(right, node))
+
+        if right.priority > root.priority
+            root = rotate_left!(root)
+        end
+    end
+    return root
+end
+
+function Base.delete!(root)
+    if lchild(root) === nothing
+        root = rchild(root)
+    elseif rchild(root) === nothing
+        root = lchild(root)
+    elseif lchild(root).priority < rchild(root).priority
+        root = rotate_left!(root)
+        setleft!(root, delete!(lchild(root)))
+    else
+        root = rotate_right!(root)
+        setright!(root, delete!(rchild(root)))
+    end
+    return root
+end
+
+function traverse(f::F, root::Node) where {F}
+    left = lchild(root)
+    if left !== nothing
+        traverse(f, left)
+    end
+    f(root)
+    right = rchild(root)
+    if right !== nothing
+        traverse(f, right)
+    end
+    return nothing
+end
+
+#####
+##### Treap
+#####
+
+mutable struct Treap{T}
+    root::Union{Nothing,Node{T}}
+    count::Int
+end
+
+root(treap::Treap) = treap.root
+function Base.push!(treap::T, key0) where {T}
+    key = convert(T, key0)
+    node = Node(key)
+    treap.root = insert!(root(treap), node)
+    treap.count += 1
+    return treap
+end
+
+#####
+##### Treap verification
+#####
+
+istree(treap::Treap) = istree(root(treap))
+function istree(node::Node{T}) where {T}
+    v = T[]
+    traverse(node) do n
+        push!(v, n.key)
+    end
+    return issorted(v)
+end
+
+isheap(treap::Treap) = isheap(root(treap))
+function isheap(node::Node)
+    passed = true
+    left = lchild(node)
+    if left !== nothing
+        passed &= isheap(left)
+        passed &= (node.priority > left.priority)
+    end
+    right = rchild(node)
+    if right !== nothing
+        passed &= isheap(right)
+        passed &= (node.priority > right.priority)
+    end
+
+    return passed
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,125 @@
 using ReuseDistance
 using Test
 
-@testset "ReuseDistance.jl" begin
-    # Write your tests here.
+@testset "Testing Node" begin
+    Node = ReuseDistance.Node
+    lchild = ReuseDistance.lchild
+    rchild = ReuseDistance.rchild
+
+    #####
+    ##### rotate_right!
+    #####
+
+    # only left child
+    node = Node(10)
+    left = Node(5)
+    ReuseDistance.setleft!(node, left)
+
+    @test lchild(node) === left
+    @test rchild(node) === nothing
+
+    newnode = ReuseDistance.rotate_right!(node)
+    @test newnode === left
+    @test rchild(left) === node
+    @test lchild(left) === nothing
+    @test rchild(node) === nothing
+    @test lchild(node) === nothing
+
+    # two children
+    node = Node(10)
+    left = Node(5)
+    right = Node(15)
+    ReuseDistance.setleft!(node, left)
+    ReuseDistance.setright!(node, right)
+
+    @test lchild(node) === left
+    @test rchild(node) === right
+
+    newnode = ReuseDistance.rotate_right!(node)
+    @test newnode === left
+    @test rchild(left) === node
+    @test lchild(left) === nothing
+    @test rchild(node) === right
+    @test lchild(node) === nothing
+
+    # hierarchy
+    node = Node(10)
+    left = Node(5)
+    leftleft = Node(1)
+    leftright = Node(7)
+
+    right = Node(20)
+
+    ReuseDistance.setleft!(node, left)
+    ReuseDistance.setleft!(left, leftleft)
+    ReuseDistance.setright!(left, leftright)
+
+    ReuseDistance.setright!(node, right)
+
+    newnode = ReuseDistance.rotate_right!(node)
+    @test newnode === left
+    @test rchild(left) === node
+    @test lchild(left) === leftleft
+
+    @test rchild(node) === right
+    @test lchild(node) === leftright
+
+    #####
+    ##### rotate_left!
+    #####
+
+    # only right child
+    node = Node(10)
+    right = Node(15)
+    ReuseDistance.setright!(node, right)
+
+    @test lchild(node) === nothing
+    @test rchild(node) === right
+
+    newnode = ReuseDistance.rotate_left!(node)
+    @test newnode === right
+    @test rchild(right) === nothing
+    @test lchild(right) === node
+    @test rchild(node) === nothing
+    @test lchild(node) === nothing
+
+    # two children
+    node = Node(10)
+    left = Node(5)
+    right = Node(15)
+    ReuseDistance.setleft!(node, left)
+    ReuseDistance.setright!(node, right)
+
+    @test lchild(node) === left
+    @test rchild(node) === right
+
+    newnode = ReuseDistance.rotate_left!(node)
+    @test newnode === right
+    @test rchild(right) === nothing
+    @test lchild(right) === node
+    @test rchild(node) === nothing
+    @test lchild(node) === left
+
+    # hierarchy
+    node = Node(10)
+    left = Node(5)
+
+    right = Node(20)
+    rightleft = Node(15)
+    rightright = Node(25)
+
+    ReuseDistance.setleft!(node, left)
+
+    ReuseDistance.setright!(node, right)
+    ReuseDistance.setleft!(right, rightleft)
+    ReuseDistance.setright!(right, rightright)
+
+    newnode = ReuseDistance.rotate_left!(node)
+    @test newnode === right
+    @test rchild(right) === rightright
+    @test lchild(right) === node
+
+    @test rchild(node) === rightleft
+    @test lchild(node) === left
 end
+
