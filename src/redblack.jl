042fa5ec8b8e9f148c9be70603ecc66262e7961a
Mark Hildebrand <mhildebrand@ucdavis.edu>
2021-10-26 15:57:45 -0700

    Checkpoint progress

--- src/redblack.jl
+++ src/redblack.jl
@@ -34,17 +34,19 @@ Unused indices are stored in the `free` field and can be used to assign new `Pro
 to items inserted into the tree.
 """
 mutable struct RedBlackTree{T}
-    root::Int
+    root::UInt
     nodes::Vector{ProtoNode{T}}
     free::Vector{Int}
 end
 
-function RedBlackTree{T}(len = 10) where {T}
+Base.length(tree::RedBlackTree) = length(tree.nodes) - length(tree.free)
+
+function RedBlackTree{T}(len::Integer = 10) where {T}
     # Due to our load-store schenanigans, make sure that we only hold `isbitstypes`.
     isbitstype(T) || error("Can only construct a RedBlackTree for `isbitstype` key types!")
 
     root = zero(Int)
-    nodes = Vector{ProtoNode{T}}(undef, len)
+    nodes = Vector{ProtoNode{T}}(undef, max(one(len), len))
     free = collect(reverse(eachindex(nodes)))
     return RedBlackTree{T}(root, nodes, free)
 end
@@ -70,7 +72,7 @@ function Base.getindex(tree::RedBlackTree)
     end
     return pop!(free)
 end
-Base.getindex(tree::RedBlackTree, index::Integer) = tree.nodes[index]
+Base.getindex(tree::RedBlackTree, index::Integer) = tree.nodes[index].key
 
 """
     return!(tree::RedBlackTree, index)
@@ -88,42 +90,23 @@ end
 @inline Base.pointer(tree::RedBlackTree, i::Integer) = pointer(tree.nodes, i)
 
 """
-    RBNode{T}
-
-The `RBNode` is a tree/index that will allow full mutation of the underlying `ProtoNode`.
-"""
-struct RBNode{T}
-    tree::RedBlackTree{T}
-    index::UInt
-end
-RBNode(node::RBNode, index::Integer) = RBNode(node.tree, index)
-
-"""
-    getparent(node::RBNode) -> Integer
-
-Return the index of `node`'s parent.
-
     getparent(tree::RedBlackTree, index::Integer) -> Integer
 
 Return the parent index for the the node at `index` in `tree`.
 """
-getparent(node::RBNode) = getparent(node.tree, node.index)
 function getparent(tree::RedBlackTree{T}, index::Integer) where {T}
     ptr = pointer(tree, index)
     unmasked = unsafe_load(Ptr{UInt}(ptr))
     return unmasked & ~msb(unmasked)
 end
 
-#setparent!(node::RBNode, v) = setparent!(node.tree, v, node.index)
 function setparent!(tree::RedBlackTree, index::Integer, v)
     ptr = Ptr{UInt}(pointer(tree, index))
     old = unsafe_load(ptr)
     new = (old & msb(old)) | (v & ~msb(old))
-    @show ptr, old, new
     return unsafe_store!(ptr, new)
 end
 
-getcolor(node::RBNode) = getcolor(node.tree, node.index)
 function getcolor(tree::RedBlackTree{T}, index::Integer) where {T}
     ptr = pointer(tree, index)
     unmasked = unsafe_load(Ptr{UInt}(ptr))
@@ -132,7 +115,6 @@ function getcolor(tree::RedBlackTree{T}, index::Integer) where {T}
     return Color(bitrotate(unmasked & msb(unmasked), 1))
 end
 
-#setcolor!(node::RBNode, color) = setcolor!(node.tree, color, node.index)
 function setcolor!(tree::RedBlackTree, index::Integer, color)
     ptr = Ptr{UInt}(pointer(tree, index))
     old = unsafe_load(ptr)
@@ -140,32 +122,27 @@ function setcolor!(tree::RedBlackTree, index::Integer, color)
     return unsafe_store!(ptr, new)
 end
 
-getchild(node::RBNode, direction::Direction) = getchild(node.tree, node.index, direction)
 function getchild(tree::RedBlackTree, index::Integer, direction::Direction)
     ptr = pointer(tree, index) + (1 + UInt(direction)) * sizeof(UInt)
     return unsafe_load(Ptr{UInt}(ptr))
 end
 
 leftchild(tree::RedBlackTree, index) = getchild(tree, index, Left)
-leftchild(node::RBNode) = leftchild(node.tree, node.index)
 rightchild(tree::RedBlackTree, index) = getchild(tree, index, Right)
-rightchild(node::RBNode) = rightchild(node.tree, node.index)
 
 function setchild!(tree::RedBlackTree, index::Integer, direction::Direction, child)
     ptr = pointer(tree, index) + (1 + UInt(direction)) * sizeof(UInt)
     return unsafe_store!(Ptr{UInt}(ptr), convert(UInt, child))
 end
 setleft!(tree::RedBlackTree, index, child) = setchild!(tree, index, Left, child)
-setleft!(node::RBNode, child) = setleft!(node.tree, node.index, child)
 setright!(tree::RedBlackTree, index, child) = setchild!(tree, index, Right, child)
-setright!(node::RBNode, child) = setright!(node.tree, node.index, child)
 
 #####
 ##### Convenience Accessors
 #####
 
 function child_direction(tree::RedBlackTree, p::Integer, n::Integer)
-    return Direction(leftchild(tree, p) == n)
+    return Direction(leftchild(tree, p) != n)
 end
 
 child_direction(tree::RedBlackTree, n::Integer) =
@@ -205,7 +182,7 @@ end
 function rotateroot!(tree::RedBlackTree, p::Integer, dir::Direction)
     g = getparent(tree, p)
     s = getchild(tree, p, reverse(dir))
-    c = getchild(tree, p, dir)
+    c = getchild(tree, s, dir)
     setchild!(tree, p, reverse(dir), c)
     iszero(c) || setparent!(tree, c, p)
 
@@ -223,12 +200,43 @@ end
 rotateleft!(tree::RedBlackTree, n::Integer) = rotateroot!(tree, n, Left)
 rotateright!(tree::RedBlackTree, n::Integer) = rotateroot!(tree, n, Right)
 
+# Now that we have the nitty-gritty accessor implementation out of the way, we can
+# get to the actual tree implementation.
+@enum Findings::UInt8 NotFoundLeft NotFoundRight Found
+function search(tree::RedBlackTree{T}, key::T) where {T}
+    n = tree.root
+    lastn = n
+    direction = Left
+
+    while !iszero(n)
+        v = tree[n]
+        key == v && return n, Found
+        direction = Direction(v < key)
+        lastn, n = n, getchild(tree, n, direction)
+    end
+    return lastn, Base.bitcast(Findings, direction)
+end
+
+function Base.haskey(tree::RedBlackTree{T}, k0) where {T}
+    _, status = search(tree, convert(T, k0))
+    return status == Found
+end
+Base.in(k, tree::RedBlackTree) = haskey(tree, k)
+
 #####
 ##### Insertion
 #####
 
-# Now that we have the nitty-gritty accessor implementation out of the way, we can
-# get to the actual tree implementation.
+function Base.push!(tree::RedBlackTree{T}, k::T) where {T}
+    p, status = search(tree, k)
+    status == Found && return tree
+
+    # Create a new node and finish inserting
+    n = tree[]
+    tree[n] = k
+    _insert!(tree, n, p, Base.bitcast(Direction, status))
+    return tree
+end
 
 """
     _insert!(tree::RedBlackTree, n, p, direction)
@@ -252,6 +260,7 @@ function _insert!(tree::RedBlackTree, n::Integer, p::Integer, direction::Directi
     setchild!(tree, p, direction, n)
 
     # Insertion procedure
+    local g
     while true
         # Case 1: Parent is Black, insertion is complete.
         if getcolor(tree, p) == Black
@@ -267,7 +276,7 @@ function _insert!(tree::RedBlackTree, n::Integer, p::Integer, direction::Directi
         end
 
         # `p` is `Red` and `g` exists.
-        direction = child_direction(tree, n, p)
+        direction = child_direction(tree, g, p)
 
         # `u` is `n`'s uncle.
         u = getchild(tree, g, reverse(direction))
@@ -283,7 +292,7 @@ function _insert!(tree::RedBlackTree, n::Integer, p::Integer, direction::Directi
         # `g` becomes the new current node.
         # Iterate one black level higher.
         n = g
-        p = getparent!(tree, n)
+        p = getparent(tree, n)
 
         # Case 3 - `p` is NULL and insertion is complete.
         iszero(p) && return nothing
@@ -303,3 +312,304 @@ function _insert!(tree::RedBlackTree, n::Integer, p::Integer, direction::Directi
     return nothing
 end
 
+#####
+##### Deletion
+#####
+
+function swap_successor!(tree::RedBlackTree, n::Integer, m::Integer)
+    # Swap colors
+    cₙ = getcolor(tree, n)
+    cₘ = getcolor(tree, m)
+    setcolor!(tree, m, cₙ)
+    setcolor!(tree, n, cₘ)
+
+    # Swap Left Children
+    lₙ = leftchild(tree, n)
+    lₘ = leftchild(tree, m)
+    setleft!(tree, n, lₘ)
+    iszero(lₘ) || setparent!(tree, lₘ, n)
+    setleft!(tree, m, lₙ)
+    iszero(lₙ) || setparent!(tree, lₙ, m)
+
+    # Need to be more careful with right children and parents for the case where
+    # `m` is the right child of `n`
+    if n == getparent(tree, m)
+        println("    Successor is child")
+        p = getparent(tree, n)
+        setparent!(tree, m, p)
+        iszero(p) || setchild!(tree, p, child_direction(tree, p, n), m)
+
+        rₘ = rightchild(tree, m)
+        setright!(tree, n, rₘ)
+        iszero(rₘ) || setparent!(tree, rₘ, n)
+        setparent!(tree, n, m)
+        setright!(tree, m, n)
+    else
+        # Swap parents
+        println("    Successor is not child")
+        pₙ = getparent(tree, n)
+        pₘ = getparent(tree, m)
+        setparent!(tree, m, pₙ)
+        iszero(pₙ) || setchild!(tree, pₙ, child_direction(tree, pₙ, n), m)
+        setparent!(tree, n, pₘ)
+        setchild!(tree, pₘ, child_direction(tree, pₘ, m), n)
+
+        # Swap right children
+        rₙ = rightchild(tree, n)
+        rₘ = rightchild(tree, m)
+        setright!(tree, n, rₘ)
+        iszero(rₘ) || setparent!(tree, rₘ, n)
+        setright!(tree, m, rₙ)
+        setparent!(tree, rₙ, m)
+    end
+    n == tree.root && (tree.root = m)
+    return nothing
+end
+
+function Base.findmin(tree::RedBlackTree, n::Integer)
+    while true
+        m = leftchild(tree, n)
+        iszero(m) && return n
+        n = m
+    end
+end
+
+function _delete!(tree::RedBlackTree, n::Integer)
+    # Case 1: `n` is the only node in the tree
+    if n == tree.root && length(tree) == 1
+        tree.root = 0
+        return nothing
+    end
+
+    # Case 2: Do we have two non-NIL children?
+    left = leftchild(tree, n)
+    right = rightchild(tree, n)
+    if !iszero(left) && !iszero(right)
+        replacement = findmin(tree, right)
+        swap_successor!(tree, n, replacement)
+
+        # Update `left` and `right`, then fall through to case 3.
+        left = leftchild(tree, n)
+        right = rightchild(tree, n)
+    end
+
+    # Case 3: `n` now has at most one non-NIL child.
+    color = getcolor(tree, n)
+    # If `n` is Red - then it has no children.
+    # Else if `n` is black, is it has a child, it must be a red child.
+    # Replace `node` with its child and color the child `Black`.
+    p = getparent(tree, n)
+    #direction = child_direction(tree, p, n)
+    if color == Red
+        println("Case 1")
+        @assert iszero(left) && iszero(right)
+        setchild!(tree, p, child_direction(tree, p, n), 0)
+        return nothing
+    elseif iszero(left) && iszero(right)
+        println("Case 2")
+        _delete_complex!(tree, n)
+        return nothing
+    else
+        println("Case 3")
+        m = iszero(left) ? right : left
+        @assert getcolor(tree, m) == Red
+        setcolor!(tree, m, Black)
+        setparent!(tree, m, p)
+        if iszero(p)
+            tree.root = m
+        else
+            setchild!(tree, p, child_direction(tree, p, n), m)
+        end
+        return nothing
+    end
+end
+
+function _delete_complex!(tree::RedBlackTree, n::Integer)
+    p = getparent(tree, n)
+    direction = child_direction(tree, p, n)
+    AbstractTrees.print_tree(tree)
+    setchild!(tree, p, direction, 0)
+
+    local s, d, c
+    while true
+        # s: sibling
+        # d: distant nephew
+        # c: close nephew
+        s = getchild(tree, p, reverse(direction))
+        d = getchild(tree, s, reverse(direction))
+        c = getchild(tree, s, direction)
+        if getcolor(tree, s) == Red
+            # `s` is `Red` implies `p, c, d` are all `Black`.
+            @goto case_d3
+        end
+        if !iszero(d) && getcolor(tree, d) == Red
+            @goto case_d6
+        end
+        if !iszero(c) && getcolor(tree, c) == Red
+            @goto case_d5
+        end
+        if getcolor(tree, p) == Red
+            @goto case_d4
+        end
+
+        # All of p, c, s, d are Black
+        setcolor!(tree, s, Red)
+        n = p
+        p = getparent(tree, n)
+        iszero(p) && return nothing
+        direction = child_direction(tree, p, n)
+    end
+
+    @label case_d3
+    println("    D3")
+    rotateroot!(tree, p, direction)
+    setcolor!(tree, p, Red)
+    setcolor!(tree, s, Black)
+    s = c
+    d = getchild(tree, s, reverse(direction))
+    if !iszero(d) && getcolor(tree, d) == Red
+        @goto case_d6
+    end
+    c = getchild(tree, s, direction)
+    if !iszero(c) && getcolor(tree, c) == Red
+        @goto case_d5
+    end
+
+    @label case_d4
+    println("    D4")
+    setcolor!(tree, s, Red)
+    setcolor!(tree, p, Black)
+    return nothing
+
+    @label case_d5
+    println("    D5")
+    rotateroot!(tree, s, reverse(direction))
+    setcolor!(tree, s, Red)
+    setcolor!(tree, c, Black)
+    d = s
+    s = c
+
+    @label case_d6
+    println("    D6")
+    rotateroot!(tree, p, direction)
+    setcolor!(tree, s, getcolor(tree, p))
+    setcolor!(tree, p, Black)
+    setcolor!(tree, d, Black)
+    return nothing
+end
+
+function Base.delete!(tree::RedBlackTree{T}, k0) where {T}
+    k = convert(T, k0)
+    n, status = search(tree, k)
+    if status == Found
+        _delete!(tree, n)
+        push!(tree.free, n)
+        return true
+    end
+    return false
+end
+
+#####
+##### misc
+#####
+
+function traverse(f::F, tree::RedBlackTree) where {F}
+    root = tree.root
+    iszero(root) || traverse(f, tree, tree.root)
+    return nothing
+end
+
+function traverse(f::F, tree::RedBlackTree, i) where {F}
+    left = leftchild(tree, i)
+    iszero(left) || traverse(f, tree, left)
+    f(i)
+    right = rightchild(tree, i)
+    iszero(right) || traverse(f, tree, right)
+    return nothing
+end
+
+function istree(tree::RedBlackTree{T}) where {T}
+    v = T[]
+    seen = Set{Int}()
+    traverse(tree) do i
+        if in(i, seen)
+            error("Tree is broken!")
+        end
+        push!(seen, i)
+        push!(v, tree[i])
+    end
+    return issorted(v)
+end
+
+function no_red_children(tree::RedBlackTree)
+    passed = true
+    traverse(tree) do i
+        if getcolor(tree, i) == Red
+            left = leftchild(tree, i)
+            right = rightchild(tree, i)
+            passed &= (iszero(left) || getcolor(tree, left) == Black)
+            passed &= (iszero(right) || getcolor(tree, right) == Black)
+        end
+    end
+    return passed
+end
+
+"""
+    pathsequal(tree::RedBlackTree)
+
+Check that all paths to all NIL decendents for each node in `tree` passes through the same
+number of black nodes.
+"""
+function pathsequal(tree::RedBlackTree)
+    root = tree.root
+    iszero(root) && return true
+    return pathlength(tree, tree.root) != 0
+end
+
+function pathlength(tree::RedBlackTree, i)
+    left = leftchild(tree, i)
+    right = rightchild(tree, i)
+    if iszero(left) && iszero(right)
+        return Int(getcolor(tree, i) == Black) + 1
+    else
+        left_length = iszero(left) ? 1 : pathlength(tree, left)
+        right_length = iszero(right) ? 1 : pathlength(tree, right)
+
+        # We've failed - abort!
+        if iszero(left_length) || iszero(right_length) || left_length != right_length
+            println("Node: $i - Left: $left_length, Right: $right_length")
+            return 0
+        else
+            return left_length + Int(getcolor(tree, i) == Black)
+        end
+    end
+end
+
+function validate(tree::RedBlackTree)
+    return istree(tree) && no_red_children(tree) && pathsequal(tree)
+end
+
+#####
+##### Abstract Trees
+#####
+
+struct TreeIndex{T,I}
+    tree::RedBlackTree{T}
+    index::I
+end
+
+AbstractTrees.print_tree(tree::RedBlackTree; kw...) =
+    AbstractTrees.print_tree(TreeIndex(tree, tree.root); kw...)
+
+function AbstractTrees.children(ti::TreeIndex)
+    left, right = leftchild(ti.tree, ti.index), rightchild(ti.tree, ti.index)
+    return [TreeIndex(ti.tree, i) for i in (left, right) if !iszero(i)]
+end
+
+AbstractTrees.printnode(io::IO, ti::TreeIndex) =
+    print(io, "$(ti.tree[ti.index]) ($(ti.index) - $(getcolor(ti.tree, ti.index)))")
+
+# [43493, 79338, 16412, 63970, 64636, 42916, 83792, 47292, 30963]#, 46520]
+#
+# adds = [60025, 61995, 80414, 78411]
+# deletes = [61995, 60024, 78411]
--- test/redblack.jl
+++ test/redblack.jl
@@ -3,5 +3,302 @@
     @test reverse(ReuseDistance.Right) == ReuseDistance.Left
 
     @testset "Testing Accessors" begin
+        # Initialize the tree with length zero.
+        # This will test the corner case handling of setting the minimum node vector
+        # length to 1 AND test the length extension logic.
+        tree = ReuseDistance.RedBlackTree{Int}(0)
+
+        # Get a new index from the tree
+        n = tree[]
+        @test n == 1
+        tree[n] = 500
+        @test tree[n] == 500
+
+        # Null all other pointer fields
+        ReuseDistance.setcolor!(tree, n, ReuseDistance.Black)
+        ReuseDistance.setparent!(tree, n, 0)
+        ReuseDistance.setleft!(tree, n, 0)
+        ReuseDistance.setright!(tree, n, 0)
+
+        @test ReuseDistance.getcolor(tree, n) == ReuseDistance.Black
+        @test ReuseDistance.getparent(tree, n) == 0
+        @test ReuseDistance.leftchild(tree, n) == 0
+        @test ReuseDistance.rightchild(tree, n) == 0
+
+        # Setting the color to `Red` should not affect the parent
+        ReuseDistance.setcolor!(tree, n, ReuseDistance.Red)
+        @test ReuseDistance.getcolor(tree, n) == ReuseDistance.Red
+        @test ReuseDistance.getparent(tree, n) == 0
+        @test ReuseDistance.leftchild(tree, n) == 0
+        @test ReuseDistance.rightchild(tree, n) == 0
+
+        # Similarly, setting the parent should not affect the color.
+        ReuseDistance.setparent!(tree, n, typemax(Int64))
+        @test ReuseDistance.getcolor(tree, n) == ReuseDistance.Red
+        @test ReuseDistance.getparent(tree, n) == typemax(Int64)
+        @test ReuseDistance.leftchild(tree, n) == 0
+        @test ReuseDistance.rightchild(tree, n) == 0
+
+        ReuseDistance.setleft!(tree, n, 10)
+        ReuseDistance.setright!(tree, n, 33)
+        @test ReuseDistance.getcolor(tree, n) == ReuseDistance.Red
+        @test ReuseDistance.getparent(tree, n) == typemax(Int64)
+        @test ReuseDistance.leftchild(tree, n) == 10
+        @test ReuseDistance.rightchild(tree, n) == 33
+
+        # Test misc accessors
+        @test ReuseDistance.child_direction(tree, n, 10) == ReuseDistance.Left
+        @test ReuseDistance.child_direction(tree, n, 33) == ReuseDistance.Right
+        @test ReuseDistance.safeparent(tree, n) == typemax(Int64)
+        @test ReuseDistance.safeparent(tree, 0) == 0
+        m = tree[]
+        @test m == 2
+        ReuseDistance.setparent!(tree, n, 0)
+        ReuseDistance.setparent!(tree, m, n)
+        ReuseDistance.setleft!(tree, n, m)
+        @test ReuseDistance.child_direction(tree, m) == ReuseDistance.Left
+
+        ReuseDistance.setleft!(tree, n, 0)
+        ReuseDistance.setright!(tree, n, m)
+        @test ReuseDistance.child_direction(tree, m) == ReuseDistance.Right
+        @test ReuseDistance.safeparent(tree, m) == n
+        @test ReuseDistance.safegrandparent(tree, m) == 0
+    end
+
+    @testset "Testing Tree Operations" begin
+        function clear!(tree, n)
+            ReuseDistance.setcolor!(tree, n, ReuseDistance.Black)
+            ReuseDistance.setparent!(tree, n, 0)
+            ReuseDistance.setleft!(tree, n, 0)
+            ReuseDistance.setright!(tree, n, 0)
+            tree[n] = 0
+            return n
+        end
+
+        # Now, we get a bit more elaborate with our tree setup, testing out tree rotations
+        # and the accessors for uncles, nephews etc.
+        #
+        # Out tree will look like this.
+        #
+        #         A
+        #        / \
+        #       B   C
+        #      / \   \
+        #     D   E   F
+        function setup_tree()
+            tree = ReuseDistance.RedBlackTree{Int}(10)
+            a = clear!(tree, tree[])
+            b = clear!(tree, tree[])
+            c = clear!(tree, tree[])
+            d = clear!(tree, tree[])
+            e = clear!(tree, tree[])
+            f = clear!(tree, tree[])
+
+            tree.root = 1
+            ReuseDistance.setleft!(tree, a, b)
+            ReuseDistance.setparent!(tree, b, a)
+
+            ReuseDistance.setright!(tree, a, c)
+            ReuseDistance.setparent!(tree, c, a)
+
+            ReuseDistance.setright!(tree, c, f)
+            ReuseDistance.setparent!(tree, f, c)
+
+            ReuseDistance.setleft!(tree, b, d)
+            ReuseDistance.setparent!(tree, d, b)
+            ReuseDistance.setright!(tree, b, e)
+            ReuseDistance.setparent!(tree, e, b)
+            return tree, (; a, b, c, d, e, f)
+        end
+
+        tree, nodes = setup_tree()
+        @test ReuseDistance.sibling(tree, nodes.b) == nodes.c
+        @test ReuseDistance.sibling(tree, nodes.c) == nodes.b
+        @test ReuseDistance.sibling(tree, nodes.f) == 0
+        @test ReuseDistance.sibling(tree, nodes.d) == nodes.e
+        @test ReuseDistance.sibling(tree, nodes.e) == nodes.d
+
+        @test ReuseDistance.getuncle(tree, nodes.d) == nodes.c
+        @test ReuseDistance.getuncle(tree, nodes.e) == nodes.c
+        @test ReuseDistance.getuncle(tree, nodes.f) == nodes.b
+
+        @test ReuseDistance.closenephew(tree, nodes.c) == nodes.e
+        @test ReuseDistance.distantnephew(tree, nodes.c) == nodes.d
+
+        @test ReuseDistance.closenephew(tree, nodes.b) == 0
+        @test ReuseDistance.distantnephew(tree, nodes.b) == nodes.f
+
+        #####
+        ##### Tree Rotations
+        #####
+
+        # After a right rotation on A
+        #
+        #         B
+        #        / \
+        #       D   A
+        #          / \
+        #         E   C
+        #              \
+        #               F
+        ReuseDistance.rotateright!(tree, nodes.a)
+        @test tree.root == nodes.b
+        @test ReuseDistance.getparent(tree, nodes.b) == 0
+
+        @test ReuseDistance.leftchild(tree, nodes.b) == nodes.d
+        @test ReuseDistance.getparent(tree, nodes.d) == nodes.b
+        @test ReuseDistance.rightchild(tree, nodes.b) == nodes.a
+        @test ReuseDistance.getparent(tree, nodes.a) == nodes.b
+
+        @test ReuseDistance.leftchild(tree, nodes.a) == nodes.e
+        @test ReuseDistance.getparent(tree, nodes.e) == nodes.a
+
+        @test ReuseDistance.rightchild(tree, nodes.a) == nodes.c
+        @test ReuseDistance.getparent(tree, nodes.c) == nodes.a
+
+        # After a left rotation on A
+        #           C
+        #          / \
+        #         A   F
+        #        /
+        #       B
+        #      / \
+        #     D   E
+        tree, nodes = setup_tree()
+        ReuseDistance.rotateleft!(tree, nodes.a)
+        @test tree.root == nodes.c
+        @test ReuseDistance.getparent(tree, nodes.c) == 0
+        @test ReuseDistance.rightchild(tree, nodes.c) == nodes.f
+        @test ReuseDistance.getparent(tree, nodes.f) == nodes.c
+
+        @test ReuseDistance.leftchild(tree, nodes.c) == nodes.a
+        @test ReuseDistance.getparent(tree, nodes.a) == nodes.c
+
+        @test ReuseDistance.rightchild(tree, nodes.a) == 0
+        @test ReuseDistance.leftchild(tree, nodes.a) == nodes.b
+        @test ReuseDistance.getparent(tree, nodes.b) == nodes.a
+
+        # Finally, perform a rotation that doesn't change the root
+        # Right rotation on `B`.
+        #
+        #
+        #         A
+        #        / \
+        #       D   C
+        #        \   \
+        #         B   F
+        #          \
+        #           E
+        tree, nodes = setup_tree()
+        ReuseDistance.rotateright!(tree, nodes.b)
+        @test tree.root == nodes.a
+        @test ReuseDistance.leftchild(tree, nodes.a) == nodes.d
+        @test ReuseDistance.getparent(tree, nodes.d) == nodes.a
+        @test ReuseDistance.leftchild(tree, nodes.d) == 0
+        @test ReuseDistance.rightchild(tree, nodes.d) == nodes.b
+        @test ReuseDistance.getparent(tree, nodes.b) == nodes.d
+
+        #####
+        ##### Search
+        #####
+
+        tree, nodes = setup_tree()
+        tree[nodes.a] = 20
+        tree[nodes.b] = 10
+        tree[nodes.c] = 30
+        tree[nodes.d] = 5
+        tree[nodes.e] = 15
+        tree[nodes.f] = 35
+
+        @test ReuseDistance.search(tree, 20) == (nodes.a, ReuseDistance.Found)
+        @test ReuseDistance.search(tree, 10) == (nodes.b, ReuseDistance.Found)
+        @test ReuseDistance.search(tree, 30) == (nodes.c, ReuseDistance.Found)
+        @test ReuseDistance.search(tree, 5) == (nodes.d, ReuseDistance.Found)
+        @test ReuseDistance.search(tree, 15) == (nodes.e, ReuseDistance.Found)
+        @test ReuseDistance.search(tree, 35) == (nodes.f, ReuseDistance.Found)
+
+        # Look for findings
+        @test ReuseDistance.search(tree, 25) == (nodes.c, ReuseDistance.NotFoundLeft)
+        @test ReuseDistance.search(tree, 16) == (nodes.e, ReuseDistance.NotFoundRight)
+    end
+
+    #####
+    ##### Insertion!
+    #####
+
+    @testset "Testing Insertion" begin
+        tree = ReuseDistance.RedBlackTree{Int}()
+        for i in 1:100
+            push!(tree, i)
+        end
+
+        @test ReuseDistance.validate(tree)
+        @test length(tree) == 100
+        @test all(in(tree), 1:100)
+        @test !any(in(tree), 101:200)
+    end
+
+    #####
+    ##### Stress Testing
+    #####
+
+    @testset "Performing Stress Test" begin
+        groundtruth = Set{Int}()
+        tree = ReuseDistance.RedBlackTree{Int}()
+        domain = 1:100_000
+        numswaps = 1000
+        increment = 0.04
+
+        swapcount = 0
+        add = true
+        while swapcount <= numswaps
+            swapchance = 0.0
+            while rand() > swapchance
+                numops = rand(1:10)
+                for _ in Base.OneTo(numops)
+                    if add
+                        i = rand(domain)
+                        while length(groundtruth) < length(domain) && in(i, groundtruth)
+                            i = rand(domain)
+                        end
+
+                        println("Adding: $i")
+                        push!(tree, i)
+                        push!(groundtruth, i)
+                    elseif !isempty(groundtruth)
+                        i = rand(groundtruth)
+                        println("Deleting: $i")
+                        ReuseDistance.AbstractTrees.print_tree(tree)
+                        delete!(tree, i)
+                        delete!(groundtruth, i)
+                    end
+
+                    ReuseDistance.validate(tree) || error()
+
+                    for i in domain
+                        if in(i, groundtruth)
+                            in(i, tree) || error("Didn't find node: $i")
+                        else
+                            !in(i, tree) || error("Found node: $i")
+                        end
+                    end
+                end
+                swapchance += increment
+            end
+
+            passed = true
+            for i in domain
+                if in(i, groundtruth)
+                    passed &= in(i, tree)
+                else
+                    passed &= !in(i, tree)
+                end
+            end
+            @test passed
+            @test ReuseDistance.validate(tree)
+            @show length(tree)
+            swapcount += 1
+            add = !add
+        end
     end
 end
